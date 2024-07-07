5a97a78f252502bf02c7805eabd1106cf729a96b
blackcorsaro <99495609+blackcorsaro@users.noreply.github.com>
2024-07-07 18:33:27 +0200

    Update Catalog.php

--- FRONT END/html/Catalog.php
+++ FRONT END/html/Catalog.php
@@ -57,12 +57,13 @@
             </div>
         </header>
 
-        <?php
-            $servername="localhost:3306";
-            $username="root";
-            $password="";
-            $database="gamevault";
-
+       <?php
+            $servername = "localhost:3306";  // Nome del server MySQL
+            $username = "root";     // Nome utente MySQL
+            $password = "";     // Password MySQL
+            $database = "gamevault";     // Nome del database
+            
+            // Creazione della connessione
             $conn = new mysqli($servername, $username, $password, $database);
             if ($conn->connect_error) {
                 die("Connessione al database fallita: " . $conn->connect_error);
@@ -72,31 +73,36 @@
             if($result->num_rows>0){
                 while ($row = $result->fetch_assoc()) {
                     $image=$row["foto"];
+                    $piattaforma=$row["piattaforma"];
                     $prezzo=$row["prezzo"];
                     $sconto=$row["sconto"];
                     $sconto_calc=($prezzo*$sconto)/100;
                     $prezzoscontato=$prezzo-$sconto_calc;
                     echo "
-                    <div class='container'>
-                        <div class='card'>
-                            <div class='imgBx'>
-                                <img src='data:image/jpeg;base64,".base64_encode($image)."'>
-                            </div>
-                            <div class='contentBx'>
-                                <h2> DOOM ETERNAL</h2>
-                                <a href='#'>Buy Now</a>
-                                <a href='#'>View More</a>
-                
-                                <p class='price'>$prezzo</p>
-                                <p class='price'>$prezzoscontato</p>
+
+                    <div id="card">
+                        <img src='data:image/jpeg;base64,".base64_encode($image)."'>
+                        <div id="content">
+                            ".$row["nome"]."
+                            <p style='text-align: center; float: left; margin-left: 35px;'> " $prezzo " </p> 
+                            <p style='text-align: center'>" $prezzoscontato "</p>
+                            <div id="price">
+                                <form action='cart.php' method='POST'>
+                                    <input type='hidden' name='nomeG' value='".$row["nome"]."'>
+                                    <button type='submit'>Buy Now</button>
+                                </form>
+                            <form action='prodotto.php' method='POST'>
+                                <input type='hidden' name='nomeG' value='".$row["nome"]."'>
+                                <button type='submit'>View More</button>
+                            </form>
                             </div>
                         </div>
-                    </div>" ;
+                    </div>
+                    " ;
                 }
             }
             $conn->close();
         ?>
-
         <section class="footerContainer">
             <div class="footer">
                 <div class="contain">
